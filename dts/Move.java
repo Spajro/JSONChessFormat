7defcea2fe191973f5028349f083ea6075f0d955
Spajro <rafal.spyra@onet.pl>
2020-06-29 20:21:06 +0200

    Move update and Trasnlator frame finished

--- dts/Move.java
+++ dts/Move.java
@@ -4,8 +4,19 @@ import java.io.Serializable;
 
 public class Move implements Serializable {
     int ox,oy,nx,ny;
-    Move(){ }
+    public Move(){
+        ox=0;
+        oy=0;
+        nx=0;
+        ny=0;
+    }
     public Move(int aox,int aoy,int anx,int any){
+        ox=aox;
+        oy=aoy;
+        nx=anx;
+        ny=any;
+    }
+    public void Update(int aox,int aoy,int anx,int any){
         ox=aox;
         oy=aoy;
         nx=anx;
--- prg/Translator.java
+++ prg/Translator.java
@@ -5,25 +5,45 @@ import dts.Move;
 
 public class Translator {
     public Move Unify_to_move(Bufor T, String M, boolean C) {
+        //TODO
+        Move Result= new Move();
         switch (M.length()) {
             case 2:
+                //ruch pionka
+                char y=M.charAt(0);
+                int x=M.charAt(1);
+                if(C){
+                    //bialy pionek
+                }
+                else{
+                    //czarny pionek
+                }
 
                 break;
 
 
             case 3:
+                //ruch figury
                 break;
 
 
             case 4:
+                //ruch jednej z mozliwych figur
                 break;
 
 
             default:
+                System.out.print("Unable to unify");
                 break;
 
         }
         return null;
+    }
+    public int Column_to_num(char C){
+        //TODO
+    }
+    public String Num_to_Fig(int F){
+        //TODO
     }
 
 }
