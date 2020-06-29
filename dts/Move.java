b6b188b1378f1623302d9085a1eab867c4633577
Spajro <rafal.spyra@onet.pl>
2020-06-29 21:11:18 +0200

    Translator Algebraic_to_move frame finished

--- dts/Move.java
+++ dts/Move.java
@@ -4,23 +4,32 @@ import java.io.Serializable;
 
 public class Move implements Serializable {
     int ox,oy,nx,ny;
+    int Roszada; //0 brak 1 bk 2 bd 3 ck 4 cd
     public Move(){
         ox=0;
         oy=0;
         nx=0;
         ny=0;
+        Roszada=0;
     }
     public Move(int aox,int aoy,int anx,int any){
         ox=aox;
         oy=aoy;
         nx=anx;
         ny=any;
+        Roszada=0;
     }
-    public void Update(int aox,int aoy,int anx,int any){
-        ox=aox;
-        oy=aoy;
-        nx=anx;
-        ny=any;
+    public Move(int R){
+        Roszada=R;
+    }
+    public void Update(int aox,int aoy,int anx,int any) {
+        ox = aox;
+        oy = aoy;
+        nx = anx;
+        ny = any;
+    }
+    public void Roszada(int R){
+        Roszada=R;
     }
     public void Make_move(Bufor T){
         T.write(T.get(ox,oy),nx,ny);
--- prg/Translator.java
+++ prg/Translator.java
@@ -4,16 +4,18 @@ import dts.Bufor;
 import dts.Move;
 
 public class Translator {
-    public Move Unify_to_move(Bufor T, String M, boolean C) {
+    public Move Algebraic_to_move(Bufor T, String M, boolean C) {
         //TODO
         Move Result= new Move();
         switch (M.length()) {
             case 2:
-                //ruch pionka
+                //ruch pionka do przodu
                 char y=M.charAt(0);
                 int x=M.charAt(1);
                 if(C){
                     //bialy pionek
+
+
                 }
                 else{
                     //czarny pionek
@@ -23,12 +25,36 @@ public class Translator {
 
 
             case 3:
-                //ruch figury
+                //ruch figury lub bicie pionem lub roszada krotka
+                if(M.equals("O-O")){
+                    //roszada
+                }
+                else if(M.charAt(0)=='x'){
+                    //bicie pionkiem
+                }
+                else{
+                    //ruch figury
+                }
                 break;
 
 
             case 4:
-                //ruch jednej z mozliwych figur
+                //ruch jednej z mozliwych figur lub bicie figurą
+                if(M.charAt(1)=='x'){
+                    //bicie figura
+                }
+                else{
+                    //ruch jednej z mozliwych figur
+                }
+                break;
+            case 5:
+                //bicie jedną z mozliwych figur lub roszada dluga
+                if(M.equals("O-O-O")){
+                    //roszada długa
+                }
+                else{
+                    //bicie jedną z mozliwych figur
+                }
                 break;
 
 
@@ -37,13 +63,15 @@ public class Translator {
                 break;
 
         }
-        return null;
+        return Result;
     }
     public int Column_to_num(char C){
         //TODO
+        return 0;
     }
     public String Num_to_Fig(int F){
         //TODO
+        return null;
     }
 
 }
