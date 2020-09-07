ad80f260d4998c8f5f76f95d17ebad503b276139
Spajro <rafal.spyra@onet.pl>
2020-09-07 22:23:37 +0200

    Checker frame added

---
+++ hlp/Checker.java
@@ -0,0 +1,16 @@
+package hlp;
+
+import dts.Bufor;
+
+public class Checker {
+    Bufor T;
+    Checker(Bufor Bf){
+        T=Bf;
+    }
+    boolean Check_Position(int tx,int ty,int FigId){
+        //TODO
+    }
+    boolean Check_Line(int tx,int ty,int StepX,int StepY){
+        //TODO
+    }
+}
