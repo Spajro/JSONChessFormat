547e3396fcfe54b214d26c8f121ca6a1018742c4
Spajro <rafal.spyra@onet.pl>
2020-09-07 23:03:06 +0200

    Checker finished

--- hlp/Checker.java
+++ hlp/Checker.java
@@ -8,9 +8,27 @@ public class Checker {
         T=Bf;
     }
     boolean Check_Position(int tx,int ty,int FigId){
-        //TODO
+        if(T.get(tx,ty)==FigId){
+            return true;
+        }
+        else return false;
     }
-    boolean Check_Line(int tx,int ty,int StepX,int StepY){
-        //TODO
+    boolean OutOfMap(int x,int y){
+        if(x<-1 || y<-1 || x>7 || y>7){
+            return false;
+        }
+        else return true;
+    }
+    boolean Check_Line(int tx,int ty,int StepX,int StepY,int FigId){
+        while(!OutOfMap(tx,ty)){
+            if(Check_Position(tx,ty,FigId)){
+                return true;
+            }
+            else{
+                tx+=StepX;
+                ty+=StepY;
+            }
+        }
+        return false;
     }
 }
