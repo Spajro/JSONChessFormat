72018d30810418a9b7a3202c03ebc69a765f1fcb
Spajro <rafal.spyra@onet.pl>
2022-07-29 21:29:26 +0200

    add Knight

--- src/data/dts/pieces/Knight.java
+++ src/data/dts/pieces/Knight.java
@@ -1,2 +1,34 @@
-package src.data.dts.pieces;public class Knight {
+package src.data.dts.pieces;
+
+import src.data.dts.Position;
+import src.data.dts.board.ChessBoard;
+import src.data.dts.color.Color;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Knight extends Piece{
+    public Knight(Color color, Position position, ChessBoard chessBoard) {
+        super(color, position, chessBoard);
+    }
+
+    @Override
+    public Set<Position> getPossibleStartPositions() {
+        return Steps.knightSteps.stream()
+                .filter(possiblePosition -> {
+                    var field=chessBoard.getField(position.add(possiblePosition));
+                    return field.isEmpty();
+                })
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<Position> getPossibleEndPositions() {
+        return Steps.knightSteps.stream()
+                .filter(possiblePosition -> {
+                    var field=chessBoard.getField(position.add(possiblePosition));
+                    return field.isEmpty() || !field.getPiece().getColor().equal(color);
+                })
+                .collect(Collectors.toSet());
+    }
 }
--- src/data/dts/pieces/Piece.java
+++ src/data/dts/pieces/Piece.java
@@ -3,10 +3,11 @@ package src.data.dts.pieces;
 import src.data.dts.Position;
 import src.data.dts.board.ChessBoard;
 import src.data.dts.color.Color;
+import src.data.dts.fields.Field;
 
 import java.util.Set;
 
-public abstract class Piece {
+public abstract class Piece implements Field {
     protected final Color color;
     protected final Position position;
     protected final ChessBoard chessBoard;
@@ -20,4 +21,22 @@ public abstract class Piece {
     public abstract Set<Position> getPossibleStartPositions();
 
     public abstract Set<Position> getPossibleEndPositions();
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Piece getPiece() {
+        return this;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
 }
