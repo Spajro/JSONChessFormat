fcbf828dd3057147a2c0f6d404687d87ee4ac9e7
Spajro <58276473+Spajro@users.noreply.github.com>
2022-07-30 20:03:20 +0200

    add Pawn

--- src/data/dts/Position.java
+++ src/data/dts/Position.java
@@ -33,4 +33,22 @@ public class Position {
     public int getY() {
         return y;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Position position = (Position) o;
+
+        if (x != position.x) return false;
+        return y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
 }
---
+++ src/data/dts/pieces/Pawn.java
@@ -0,0 +1,101 @@
+package src.data.dts.pieces;
+
+import src.data.dts.Position;
+import src.data.dts.board.ChessBoard;
+import src.data.dts.color.Color;
+import src.data.dts.fields.Field;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Pawn extends Piece {
+    private enum Step {
+        FRONT, LEFT, RIGHT, FAR
+    }
+
+    private final HashMap<Step, Position> stepsForward = new HashMap<>();
+    private final HashMap<Step, Position> stepsBackward = new HashMap<>();
+
+    Pawn(Color color, Position position, ChessBoard chessBoard) {
+        super(color, position, chessBoard);
+        if (color.isWhite()) {
+            populateWhite(stepsForward);
+            populateBlack(stepsBackward);
+        }
+        if (color.isBlack()) {
+            populateWhite(stepsBackward);
+            populateBlack(stepsForward);
+        }
+    }
+
+    @Override
+    public Set<Position> getPossibleStartPositions() {
+        Set<Position> result = stepsBackward.values().stream()
+                .filter(value -> !value.equals(stepsBackward.get(Step.FAR)))
+                .filter(value -> chessBoard.getField(value).isEmpty())
+                .collect(Collectors.toSet());
+        if (isOnStartLine(position.add(stepsBackward.get(Step.FAR)))) {
+            result.add(position.add(stepsBackward.get(Step.FAR)));
+        }
+        return result;
+    }
+
+    @Override
+    public Set<Position> getPossibleEndPositions() {
+        Set<Position> result = new HashSet<>();
+        if (!isPieceInFront()) {
+            result.add(position.add(stepsForward.get(Step.FRONT)));
+        }
+        if (canCaptureRight()) {
+            result.add((position.add(stepsForward.get(Step.RIGHT))));
+        }
+        if (canCaptureLeft()) {
+            result.add((position.add(stepsForward.get(Step.LEFT))));
+        }
+        if (isOnStartLine(position)) {
+            result.add(position.add(stepsForward.get(Step.FAR)));
+        }
+        return result;
+    }
+
+    private boolean isOnStartLine(Position temporaryPosition) {
+        return (temporaryPosition.getY() == 2 && color.isWhite()) || (temporaryPosition.getY() == 7 && color.isBlack());
+    }
+
+    private boolean canCaptureLeft() {
+        return isEnemyPieceOnStep(Step.LEFT);
+    }
+
+    private boolean canCaptureRight() {
+        return isEnemyPieceOnStep(Step.RIGHT);
+    }
+
+    private boolean isEnemyPieceOnStep(Step step) {
+        Field field = chessBoard.getField(position.add(stepsForward.get(step)));
+        if (field.isEmpty()) {
+            return false;
+        }
+        Piece piece = field.getPiece();
+        return !piece.getColor().equal(color);
+    }
+
+    private boolean isPieceInFront() {
+        return !chessBoard.getField(position.add(stepsForward.get(Step.FRONT))).isEmpty();
+    }
+
+    private void populateWhite(HashMap<Step, Position> map) {
+        map.put(Step.FRONT, new Position(0, 1));
+        map.put(Step.LEFT, new Position(-1, 1));
+        map.put(Step.RIGHT, new Position(1, 1));
+        map.put(Step.FAR, new Position(0, 2));
+    }
+
+    private void populateBlack(HashMap<Step, Position> map) {
+        map.put(Step.FRONT, new Position(0, -1));
+        map.put(Step.LEFT, new Position(-1, -1));
+        map.put(Step.RIGHT, new Position(1, -1));
+        map.put(Step.FAR, new Position(0, -2));
+    }
+}
