acf1bd39826c801d8e818de61802dd5cf16d3d96
Spajro <58276473+Spajro@users.noreply.github.com>
2022-07-31 23:37:58 +0200

    added rules in ChessBoard

--- src/data/dts/board/ChessBoard.java
+++ src/data/dts/board/ChessBoard.java
@@ -16,15 +16,18 @@ import java.util.stream.Collectors;
 public class ChessBoard {
     private final Board board;
     private final Color color;
+    private final CastleRequirements castleRequirements;
 
     public ChessBoard() {
         board = Board.getStart();
         color = Color.white;
+        castleRequirements = new CastleRequirements();
     }
 
-    private ChessBoard(Board board, Color color) {
+    private ChessBoard(Board board, Color color, CastleRequirements castleRequirements) {
         this.board = board;
         this.color = color;
+        this.castleRequirements = castleRequirements;
     }
 
     @Deprecated
@@ -41,7 +44,7 @@ public class ChessBoard {
             if (isLegal(move)) {
                 Board tempBoard = Board.getCopy(board);
                 move.makeMove(tempBoard);
-                return new ChessBoard(tempBoard, color.swap());
+                return new ChessBoard(tempBoard, color.swap(), getNextRequirements(move));
             }
         } catch (ChessAxiomViolation e) {
             throw new RuntimeException(e);
@@ -51,11 +54,68 @@ public class ChessBoard {
     }
 
     private boolean isLegal(Move move) throws ChessAxiomViolation {
-        return Board.getPieceColor(board.read(move.getOldPosition())).equal(color)
-                && move.isCorrect()
-                && !getField(move.getOldPosition()).isEmpty()
-                && getField(move.getOldPosition()).getPiece().getPossibleEndPositions().contains(move.getNewPosition())
-                && (!isKingChecked(color) ||  (BoardWrapper.getFieldFromBoard(this,board,move.getOldPosition()) instanceof King && !isPositionAttacked(move.getNewPosition())));
+        if (!Board.getPieceColor(board.read(move.getOldPosition())).equal(color)
+                || !move.isCorrect()
+                || getField(move.getOldPosition()).isEmpty()) {
+            return false;
+        }
+        if (move.isCastle()) {
+            return isCastleLegal(move.getCastle());
+        } else
+            return getField(move.getOldPosition()).getPiece().getPossibleEndPositions().contains(move.getNewPosition())
+                    && (!isKingChecked(color) || (BoardWrapper.getFieldFromBoard(this, board, move.getOldPosition()) instanceof King && !isPositionAttacked(move.getNewPosition())));
+    }
+
+    private boolean isCastleLegal(int castle) {
+        return switch (castle) {
+            case Move.WHITE_SHORT_CASTLE -> castleRequirements.canCastleWhiteShort();
+            case Move.WHITE_LONG_CASTLE -> castleRequirements.canCastleWhiteLong();
+            case Move.BLACK_SHORT_CASTLE -> castleRequirements.canCastleBlackShort();
+            case Move.BLACK_LONG_CASTLE -> castleRequirements.canCastleBlackLong();
+            default -> throw new IllegalArgumentException("Wrong castle type");
+        };
+    }
+
+    private CastleRequirements getNextRequirements(Move move) {
+        if (kingMoved(move)) {
+            return castleRequirements.kingMoved(color);
+        } else if (hColumnRookMoved(move)) {
+            return castleRequirements.hColumnRookMoved(color);
+        } else if (aColumnRookMoved(move)) {
+            return castleRequirements.hColumnRookMoved(color);
+        } else {
+            return castleRequirements.copy();
+        }
+    }
+
+    private boolean aColumnRookMoved(Move move) {
+        return isRookOnPosition(move.getOldPosition()) && move.getOldPosition().getX() == 1 && move.getOldPosition().getY() == getStartRow();
+    }
+
+    private boolean hColumnRookMoved(Move move) {
+        return isRookOnPosition(move.getOldPosition()) && move.getOldPosition().getX() == 8 && move.getOldPosition().getY() == getStartRow();
+    }
+
+    private boolean kingMoved(Move move) {
+        return isKingOnPosition(move.getOldPosition()) && move.getOldPosition().getX() == 5 && move.getOldPosition().getY() == getStartRow();
+    }
+
+    private boolean isRookOnPosition(Position position) {
+        Field field = BoardWrapper.getFieldFromBoard(this, board, position);
+        if (field.isEmpty()) {
+            return false;
+        }
+        Piece piece = field.getPiece();
+        return (piece instanceof King) && piece.getColor().equal(color);
+    }
+
+    private boolean isKingOnPosition(Position position) {
+        Field field = BoardWrapper.getFieldFromBoard(this, board, position);
+        if (field.isEmpty()) {
+            return false;
+        }
+        Piece piece = field.getPiece();
+        return (piece instanceof King) && piece.getColor().equal(color);
     }
 
     public Color getColor() {
@@ -91,16 +151,16 @@ public class ChessBoard {
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 
-    public List<Piece> getPiecesOfColor(Color color) {
+    private List<Piece> getPiecesOfColor(Color color) {
         return getAllPositions().stream()
                 .map(this::getField)
                 .filter(Field::hasPiece)
                 .map(Field::getPiece)
-                .filter(piece-> piece.getColor().equal(color))
+                .filter(piece -> piece.getColor().equal(color))
                 .toList();
     }
 
-    public List<Position> getAllPositions() {
+    private List<Position> getAllPositions() {
         List<Position> result = new LinkedList<>();
         for (int x = 1; x <= 8; x++) {
             for (int y = 1; y <= 8; y++) {
@@ -110,19 +170,27 @@ public class ChessBoard {
         return result;
     }
 
-    public boolean isKingChecked(Color color) throws ChessAxiomViolation {
-        Optional<Piece> optionalKing =getPiecesOfColor(color).stream()
-                .filter(piece->piece instanceof King)
+    private boolean isKingChecked(Color color) throws ChessAxiomViolation {
+        Optional<Piece> optionalKing = getPiecesOfColor(color).stream()
+                .filter(piece -> piece instanceof King)
                 .findFirst();
-        if(optionalKing.isEmpty()){
+        if (optionalKing.isEmpty()) {
             throw new ChessAxiomViolation("No King on Board");
         }
-        King king= (King) optionalKing.get();
+        King king = (King) optionalKing.get();
 
         return isPositionAttacked(king.getPosition());
     }
 
-    public boolean isPositionAttacked(Position position){
-        return getNumberOfPiecesAttackingFields(color).get(position)>0;
+    private boolean isPositionAttacked(Position position) {
+        return getNumberOfPiecesAttackingFields(color).get(position) > 0;
+    }
+
+    private int getStartRow() {
+        if (color.isWhite()) {
+            return 1;
+        } else {
+            return 8;
+        }
     }
 }
