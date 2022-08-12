78824cc6eadbdbb452dbd7edf178b80ffa40f4e2
Spajro <58276473+Spajro@users.noreply.github.com>
2022-08-12 20:38:29 +0200

    Add tests for Knight

---
+++ src/test/java/chess/pieces/KnightTest.java
@@ -0,0 +1,67 @@
+package chess.pieces;
+
+import chess.Position;
+import chess.board.ChessBoard;
+import chess.color.Color;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KnightTest {
+
+    @Test
+    void shouldReturn8PositionsForKnightInMiddle(){
+        ChessBoard chessBoard=ChessBoard.getBlank(Color.white);
+        Knight knight=new Knight(Color.white,new Position(4,4),chessBoard);
+        assertEquals(knight.getPossibleEndPositions().size(),8);
+        assertEquals(knight.getPossibleStartPositions().size(),8);
+    }
+
+    @Test
+    void shouldReturn2PositionsForKnightInCorner(){
+        ChessBoard chessBoard=ChessBoard.getBlank(Color.white);
+        Knight knight=new Knight(Color.white,new Position(1,1),chessBoard);
+        assertEquals(knight.getPossibleEndPositions().size(),2);
+        assertEquals(knight.getPossibleStartPositions().size(),2);
+    }
+
+    @Test
+    void shouldReturn4PositionsForKnightOnMiddleOfEdge(){
+        ChessBoard chessBoard=ChessBoard.getBlank(Color.white);
+        Knight knight=new Knight(Color.white,new Position(8,4),chessBoard);
+        assertEquals(knight.getPossibleEndPositions().size(),4);
+        assertEquals(knight.getPossibleStartPositions().size(),4);
+    }
+
+    @Test
+    void shouldReturn3PositionsForKnightOnEdgeCloseToCorner(){
+        ChessBoard chessBoard=ChessBoard.getBlank(Color.white);
+        Knight knight=new Knight(Color.white,new Position(8,2),chessBoard);
+        assertEquals(knight.getPossibleEndPositions().size(),3);
+        assertEquals(knight.getPossibleStartPositions().size(),3);
+    }
+
+    @Test
+    void shouldReturn8PositionsWhenAttackEnemyPieceFromMiddle(){
+        ChessBoard chessBoard1=ChessBoard.getBlank(Color.white);
+        ChessBoard chessBoard2=chessBoard1.put(new Pawn(Color.black,new Position(3,2),chessBoard1));
+        Knight knight=new Knight(Color.white,new Position(4,4),chessBoard2);
+        assertEquals(knight.getPossibleEndPositions().size(),8);
+    }
+
+    @Test
+    void shouldReturn7PositionsWhenTryToGoBackToFieldWithPiece(){
+        ChessBoard chessBoard1=ChessBoard.getBlank(Color.white);
+        ChessBoard chessBoard2=chessBoard1.put(new Pawn(Color.black,new Position(3,2),chessBoard1));
+        Knight knight=new Knight(Color.white,new Position(4,4),chessBoard2);
+        assertEquals(knight.getPossibleStartPositions().size(),7);
+    }
+
+    @Test
+    void shouldReturn7PositionsWhenAttackOwnPieceFromMiddle(){
+        ChessBoard chessBoard1=ChessBoard.getBlank(Color.white);
+        ChessBoard chessBoard2=chessBoard1.put(new Pawn(Color.white,new Position(3,2),chessBoard1));
+        Knight knight=new Knight(Color.white,new Position(4,4),chessBoard2);
+        assertEquals(knight.getPossibleEndPositions().size(),7);
+    }
+}
