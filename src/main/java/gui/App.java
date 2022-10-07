1ca1ddcf2a01750269921b88a2e6d3f4e4d892ce
Spajro <Spajro@users.noreply.github.com>
2022-10-07 23:45:58 +0200

    clean up App

--- src/main/java/gui/App.java
+++ src/main/java/gui/App.java
@@ -1,7 +1,6 @@
 package gui;
 
 import data.model.DataModel;
-import data.model.Diagram;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,41 +12,45 @@ public class App {
     private final OptionPanel optionPanel;
     private final Controller controller;
     private final JMenuBar menuBar;
+
     public App(DataModel dataModel) {
-        frame=new JFrame();
-        menuBar=new JMenuBar();
+        frame = new JFrame();
+        menuBar = new JMenuBar();
         createFileMenu(dataModel);
-        frame.setJMenuBar(menuBar);
-        boardPanel=new BoardPanel(dataModel.getActualNode());
-        controller=new Controller(dataModel, boardPanel);
-        optionPanel=new OptionPanel(controller);
-        boardPanel.addMouseListener(new BoardMouseListener(controller));
-        boardPanel.setFocusable(true);
-        boardPanel.addKeyListener(new KeyboardListener(controller));
-        boardPanel.setSize(480,480);
-        //optionPanel.setSize(160,480);
-        frame.add(boardPanel,BorderLayout.CENTER);
-        frame.add(optionPanel,BorderLayout.EAST);
-        frame.setSize(640,480);
-        frame.setVisible(true);
-    }
-    public void setDiagram(Diagram diagram){
-        boardPanel.setDiagram(diagram);
+        boardPanel = new BoardPanel(dataModel.getActualNode());
+        controller = new Controller(dataModel, boardPanel);
+        optionPanel = new OptionPanel(controller);
+        setUpBoardPanel();
+        setUpFrame();
     }
 
-    private void createFileMenu(DataModel dataModel){
-        JMenu fileMenu= new JMenu("File");
-        JMenuItem saveMenuItem=new JMenuItem("Save");
+    private void setUpFrame() {
+        frame.setJMenuBar(menuBar);
+        frame.add(boardPanel, BorderLayout.CENTER);
+        frame.add(optionPanel, BorderLayout.EAST);
+        frame.setSize(640, 480);
+        frame.setVisible(true);
+    }
+
+    private void createFileMenu(DataModel dataModel) {
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem saveMenuItem = new JMenuItem("Save");
         saveMenuItem.addActionListener(e -> dataModel.saveDataToFile(getFilename()));
-        JMenuItem loadMenuItem=new JMenuItem("Load");
+        JMenuItem loadMenuItem = new JMenuItem("Load");
         loadMenuItem.addActionListener(e -> dataModel.loadDataFromFile(getFilename()));
         fileMenu.add(saveMenuItem);
         fileMenu.add(loadMenuItem);
         menuBar.add(fileMenu);
     }
 
+    private void setUpBoardPanel() {
+        boardPanel.addMouseListener(new BoardMouseListener(controller));
+        boardPanel.setFocusable(true);
+        boardPanel.addKeyListener(new KeyboardListener(controller));
+        boardPanel.setSize(480, 480);
+    }
 
-    private String getFilename(){
-        return JOptionPane.showInputDialog(frame,"What is name of file?",null);
+    private String getFilename() {
+        return JOptionPane.showInputDialog(frame, "What is name of file?", null);
     }
 }
