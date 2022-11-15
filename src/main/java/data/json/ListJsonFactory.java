49a477ae1986f7d1ce82a0563ee8339eaef7b488
Spajro <58276473+Spajro@users.noreply.github.com>
2022-11-15 16:37:20 +0100

    fix json format

--- src/main/java/data/json/ListJsonFactory.java
+++ src/main/java/data/json/ListJsonFactory.java
@@ -6,6 +6,9 @@ public class ListJsonFactory {
     public static <T extends Jsonable> String listToJson(List<T> list) {
         StringBuilder result = new StringBuilder("[");
         list.forEach(e -> result.append(e.toJson()).append(","));
+        if (list.size() > 0) {
+            result.deleteCharAt(result.length() - 1);
+        }
         result.append("]");
         return result.toString();
     }
