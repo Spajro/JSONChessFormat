c3f35241b54767d98d52907870e07fc99e5f0d84
Spajro <rafal.spyra@onet.pl>
2023-03-06 10:18:53 +0100

    add time measuring to PerftTest

--- src/test/java/chess/PerftTest.java
+++ src/test/java/chess/PerftTest.java
@@ -19,51 +19,61 @@ public class PerftTest {
         }
     }
 
+    long timedPerft(ChessBoard board, int n) {
+        long startTime = System.nanoTime();
+        long result = perft(board, n);
+        long endTime = System.nanoTime();
+
+        long nanoDuration = (endTime - startTime);
+        double secondDuration = ((double) nanoDuration / Math.pow(10, 9));
+        double nodesPerSec = ((double) result / secondDuration);
+        System.out.println("Perft(" + n + ") in time " + secondDuration + "s and " + result + " nodes resulting with " + nodesPerSec + "nps");
+        return result;
+    }
+
     double deviation(long expected, long actual) {
         return ((float) Math.abs(expected - actual)) / expected;
     }
 
     @Test
     void perft1Test() {
-        long score = perft(new ChessBoard(), 1);
+        long score = timedPerft(new ChessBoard(), 1);
         System.out.println("Deviation for n = " + 1 + " is " + deviation(20, score));
         assertEquals(20, score);
     }
 
     @Test
     void perft2Test() {
-        long score = perft(new ChessBoard(), 2);
+        long score = timedPerft(new ChessBoard(), 2);
         System.out.println("Deviation for n = " + 2 + " is " + deviation(400, score));
         assertEquals(400, score);
     }
 
     @Test
     void perft3Test() {
-        long score = perft(new ChessBoard(), 3);
+        long score = timedPerft(new ChessBoard(), 3);
         System.out.println("Deviation for n = " + 3 + " is " + deviation(8902, score));
         assertEquals(8902, score);
     }
 
     @Test
     void perft4Test() {
-        long score = perft(new ChessBoard(), 4);
+        long score = timedPerft(new ChessBoard(), 4);
         System.out.println("Deviation for n = " + 4 + " is " + deviation(197281, score));
         assertEquals(197281, score);
     }
 
     @Test
     void perft5Test() {
-        long score = perft(new ChessBoard(), 5);
+        long score = timedPerft(new ChessBoard(), 5);
         System.out.println("Deviation for n = " + 5 + " is " + deviation(4865609, score));
         assertEquals(4865609, score);
     }
 
     @Test
     void perft6Test() {
-        long score = perft(new ChessBoard(), 6);
+        long score = timedPerft(new ChessBoard(), 6);
         System.out.println("Deviation for n = " + 6 + " is " + deviation(119060324, score));
         assertEquals(119060324, score);
     }
-
-
 }
