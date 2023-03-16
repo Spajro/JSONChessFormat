3c607b444dd10a34f6a502bf2cb3373e4bc20c1e
Spajro <rafal.spyra@onet.pl>
2023-03-16 15:42:08 +0100

    move textArea to scrollPane

--- src/main/java/gui/option/OptionPanel.java
+++ src/main/java/gui/option/OptionPanel.java
@@ -3,27 +3,25 @@ package gui.option;
 import gui.Controller;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class OptionPanel extends JPanel {
     private final JTree jTree;
-    private final JScrollPane jScrollPane;
     private final JTextArea jTextArea;
+
     public OptionPanel(Controller controller) {
-        setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
-        jTree=controller.createTreeWithDataModel();
-        jTree.addMouseListener(new TreeMouseListener(jTree,controller));
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        jTree = controller.createTreeWithDataModel();
+        jTree.addMouseListener(new TreeMouseListener(jTree, controller));
 
-        jScrollPane=new JScrollPane(jTree);
+        jTextArea = new JTextArea();
 
-        jTextArea=new JTextArea();
         jTextArea.getDocument().addDocumentListener(new TextAreaListener(controller));
 
-        add(jScrollPane);
-        add(jTextArea);
+        add(new JScrollPane(jTree));
+        add(new JScrollPane(jTextArea));
     }
 
-    public void setText(String text){
+    public void setText(String text) {
         jTextArea.setText(text);
     }
 }
