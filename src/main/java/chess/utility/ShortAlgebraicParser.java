8bc9fa79f0c3ad63247e683dae29892e71ae731b
Spajro <Spajro@users.noreply.github.com>
2023-04-10 14:42:19 +0200

    add handling ambiguous moves in ShortAlgebraicParser

--- src/main/java/chess/utility/ShortAlgebraicParser.java
+++ src/main/java/chess/utility/ShortAlgebraicParser.java
@@ -16,7 +16,7 @@ public class ShortAlgebraicParser {
     public RawMove parseShortAlgebraic(String move, ChessBoard chessBoard) {
         return utility.algebraicCastleToMove(move, chessBoard.getColor())
                 .orElseGet(() -> shortAlgebraicToMove(move, chessBoard)
-                        .orElseThrow(() -> new IllegalArgumentException("Wrong algebraic"))
+                        .orElseThrow(() -> new IllegalArgumentException("Illegal short algebraic: " + move))
                 );
     }
 
@@ -73,7 +73,17 @@ public class ShortAlgebraicParser {
     }
 
     private Optional<RawMove> specialPawnToMove(String move, ChessBoard chessBoard) {
-        return Optional.empty();//TODO
+        Position end = utility.algebraicToPosition(move.substring(1));
+        Piece pawn = new Pawn(chessBoard.getColor(), end, chessBoard);
+        Set<Position> positionSet = getPositions(pawn, chessBoard);
+        if (positionSet.size() < 1) {
+            return Optional.empty();
+        }
+        Optional<Position>optionalStart=chooseBySpecial(positionSet, move.charAt(0));
+        if(optionalStart.isEmpty()){
+            return Optional.empty();
+        }
+        return Optional.of(new RawMove(optionalStart.get(), end));
     }
 
     private Optional<RawMove> pieceCaptureToMove(String move, ChessBoard chessBoard) {
@@ -84,19 +94,73 @@ public class ShortAlgebraicParser {
     }
 
     private Optional<RawMove> specialPieceToMove(String move, ChessBoard chessBoard) {
-        return Optional.empty();//TODO
+        Position end = utility.algebraicToPosition(move.substring(2));
+        Optional<Piece> optionalPiece = charToPiece(move.charAt(0), end, chessBoard);
+        if (optionalPiece.isEmpty()) {
+            return Optional.empty();
+        }
+        Piece piece = optionalPiece.get();
+        Set<Position> positionSet = getPositions(piece, chessBoard);
+        if (positionSet.size() < 1) {
+            return Optional.empty();
+        }
+        Optional<Position>optionalStart=chooseBySpecial(positionSet, move.charAt(0));
+        if(optionalStart.isEmpty()){
+            return Optional.empty();
+        }
+        return Optional.of(new RawMove(optionalStart.get(), end));
     }
 
     private Optional<RawMove> specialPawnCaptureToMove(String move, ChessBoard chessBoard) {
-        return Optional.empty();//TODO
+        if (move.charAt(1) != 'x') {
+            return Optional.empty();
+        }
+        return specialPieceToMove(move.charAt(0) + move.substring(2), chessBoard);
     }
 
     private Optional<RawMove> specialPieceCaptureToMove(String move, ChessBoard chessBoard) {
-        return Optional.empty();//TODO
+        if (move.charAt(2) != 'x') {
+            return Optional.empty();
+        }
+        return specialPieceToMove(move.substring(0, 2) + move.substring(3), chessBoard);
     }
 
     private Optional<RawMove> getSinglePieceMove(Piece piece, ChessBoard chessBoard) {
-        Set<Position> positionSet = piece
+        Set<Position> positionSet = getPositions(piece, chessBoard);
+        if (positionSet.size() != 1) {
+            return Optional.empty();
+        }
+        return Optional.of(new RawMove(positionSet.stream().findAny().get(), piece.getPosition()));
+    }
+
+    private Optional<Position> chooseBySpecial(Set<Position> positions, char special) {
+        try {
+            int column = utility.columnToNumber(special);
+            Set<Position> positionSet = positions.stream()
+                    .filter(position -> position.getX() == column)
+                    .collect(Collectors.toSet());
+            if (positionSet.size() != 1) {
+                return Optional.empty();
+            }
+            return positionSet.stream().findAny();
+        } catch (IllegalArgumentException ignored) {
+        }
+        try {
+            int row = rowToNumber(special);
+            Set<Position> positionSet = positions.stream()
+                    .filter(position -> position.getY() == row)
+                    .collect(Collectors.toSet());
+            if (positionSet.size() != 1) {
+                return Optional.empty();
+            }
+            return positionSet.stream().findAny();
+        } catch (IllegalArgumentException ignored) {
+        }
+        return Optional.empty();
+    }
+
+    private static Set<Position> getPositions(Piece piece, ChessBoard chessBoard) {
+        return piece
                 .getPossibleStartPositions()
                 .stream()
                 .map(chessBoard::getField)
@@ -105,10 +169,6 @@ public class ShortAlgebraicParser {
                 .filter(piece::partiallyEquals)
                 .map(Piece::getPosition)
                 .collect(Collectors.toSet());
-        if (positionSet.size() != 1) {
-            return Optional.empty();
-        }
-        return Optional.of(new RawMove(positionSet.stream().findAny().get(), piece.getPosition()));
     }
 
     private Optional<Piece> charToPiece(char piece, Position position, ChessBoard chessBoard) {
@@ -120,5 +180,19 @@ public class ShortAlgebraicParser {
             case 'K' -> new King(chessBoard.getColor(), position, chessBoard);
             default -> null;
         });
+    }
+
+    private int rowToNumber(char row) {
+        return switch (row) {
+            case '1' -> 1;
+            case '2' -> 2;
+            case '3' -> 3;
+            case '4' -> 4;
+            case '5' -> 5;
+            case '6' -> 6;
+            case '7' -> 7;
+            case '8' -> 8;
+            default -> throw new IllegalArgumentException("Not valid row:" + row);
+        };
     }
 }
