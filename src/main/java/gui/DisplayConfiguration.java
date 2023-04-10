a75d054e925231aa29880227504176e6c007fe4a
Spajro <Spajro@users.noreply.github.com>
2023-04-10 19:15:54 +0200

    DisplayConfiguration cleanup

--- src/main/java/gui/DisplayConfiguration.java
+++ src/main/java/gui/DisplayConfiguration.java
@@ -6,34 +6,38 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
-import java.net.URL;
+import java.io.InputStream;
 import java.util.HashMap;
 
 public class DisplayConfiguration {
-    public static final String FOLDERPATH = "/pieces/";
-    public static final String WPAWNPATH = "white/pawn";
-    public static final String WKNIGHTPATH = "white/knight";
-    public static final String WBISHOPPATH = "white/bishop";
-    public static final String WROOKPATH = "white/rook";
-    public static final String WQUEENPATH = "white/queen";
-    public static final String WKINGPATH = "white/king";
-    public static final String BPAWNPATH = "black/pawn";
-    public static final String BKNIGHTPATH = "black/knight";
-    public static final String BBISHOPPATH = "black/bishop";
-    public static final String BROOKPATH = "black/rook";
-    public static final String BQUEENPATH = "black/queen";
-    public static final String BKINGPATH = "/black/king";
-    public static final String FORMAT = ".bmp";
-    HashMap<Piece.Type, ImageIcon> whiteImageMap = new HashMap<>();
-    HashMap<Piece.Type, ImageIcon> blackImageMap = new HashMap<>();
+    private static final String FOLDERPATH = "/pieces/";
+    private static final String WPAWNPATH = "white/pawn";
+    private static final String WKNIGHTPATH = "white/knight";
+    private static final String WBISHOPPATH = "white/bishop";
+    private static final String WROOKPATH = "white/rook";
+    private static final String WQUEENPATH = "white/queen";
+    private static final String WKINGPATH = "white/king";
+    private static final String BPAWNPATH = "black/pawn";
+    private static final String BKNIGHTPATH = "black/knight";
+    private static final String BBISHOPPATH = "black/bishop";
+    private static final String BROOKPATH = "black/rook";
+    private static final String BQUEENPATH = "black/queen";
+    private static final String BKINGPATH = "/black/king";
+    private static final String FORMAT = ".bmp";
+    private final HashMap<Piece.Type, ImageIcon> whiteImageMap;
+    private final HashMap<Piece.Type, ImageIcon> blackImageMap;
 
     public DisplayConfiguration() {
+        whiteImageMap = new HashMap<>();
+        blackImageMap = new HashMap<>();
+
         whiteImageMap.put(Piece.Type.PAWN, getImageFromResources(WPAWNPATH));
         whiteImageMap.put(Piece.Type.KNIGHT, getImageFromResources(WKNIGHTPATH));
         whiteImageMap.put(Piece.Type.BISHOP, getImageFromResources(WBISHOPPATH));
         whiteImageMap.put(Piece.Type.ROOK, getImageFromResources(WROOKPATH));
         whiteImageMap.put(Piece.Type.KING, getImageFromResources(WKINGPATH));
         whiteImageMap.put(Piece.Type.QUEEN, getImageFromResources(WQUEENPATH));
+
         blackImageMap.put(Piece.Type.PAWN, getImageFromResources(BPAWNPATH));
         blackImageMap.put(Piece.Type.KNIGHT, getImageFromResources(BKNIGHTPATH));
         blackImageMap.put(Piece.Type.BISHOP, getImageFromResources(BBISHOPPATH));
@@ -43,12 +47,8 @@ public class DisplayConfiguration {
     }
 
     private ImageIcon getImageFromResources(String path) {
-        URL url = DisplayConfiguration.class.getResource(fullPieceImagePath(path));
-        try {
-            if (url == null) {
-                throw new RuntimeException("Cant read piece from resources");
-            }
-            Image image = ImageIO.read(url);
+        try (InputStream resource = DisplayConfiguration.class.getResourceAsStream(fullPieceImagePath(path))) {
+            Image image = ImageIO.read(resource);
             return new ImageIcon(image);
         } catch (IOException e) {
             throw new RuntimeException(e);
