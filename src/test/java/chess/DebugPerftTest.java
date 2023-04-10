07425fe6cf3904db899d897957f28a2f97ae76f4
Spajro <Spajro@users.noreply.github.com>
2023-04-10 17:35:46 +0200

    add DebugPerftTest.divide

--- src/test/java/chess/DebugPerftTest.java
+++ src/test/java/chess/DebugPerftTest.java
@@ -3,6 +3,7 @@ package chess;
 import chess.board.ChessBoard;
 import chess.moves.*;
 import chess.utility.FENParser;
+import chess.utility.LongAlgebraicFactory;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -11,24 +12,42 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @Disabled
 public class DebugPerftTest {
     Map<String, Long> debugPerft(ChessBoard board, int n) {
         if (n == 1) {
             return listToMoveTypesMap(board.getGenerator().getAllPossibleExecutableMoves());
         } else {
-            Map<String, Long> result=Map.of("S",0L,"E",0L,"C",0L,"P",0L);
-            List<Map<String, Long>> list= board.getGenerator().getAllPossibleExecutableMoves().stream()
+            Map<String, Long> result = Map.of("S", 0L, "E", 0L, "C", 0L, "P", 0L);
+            List<Map<String, Long>> list = board.getGenerator().getAllPossibleExecutableMoves().stream()
                     .map(move -> debugPerft(board.makeMove(move), n - 1)).toList();
             for (Map<String, Long> map : list) {
-                result=add(result,map);
+                result = add(result, map);
             }
             return result;
         }
     }
 
+    long divide(ChessBoard board, int n, boolean print) {
+        LongAlgebraicFactory longAlgebraicFactory = new LongAlgebraicFactory();
+        if (n < 2) {
+            return -1;
+        }
+        long result = 0;
+        for (ExecutableMove move : board.getGenerator().getAllPossibleExecutableMoves()) {
+            long temp = divide(board.makeMove(move), n - 1, false);
+            if (print) {
+                System.out.println(longAlgebraicFactory.moveToLongAlgebraic(board, move) + " : " + temp);
+            }
+            result += temp;
+        }
+        return result;
+    }
+
     Map<String, Long> listToMoveTypesMap(List<ExecutableMove> moves) {
-        Map<String, Long> map= moves.stream()
+        Map<String, Long> map = moves.stream()
                 .map(move -> {
                     if (move instanceof SimpleMove) {
                         return "S";
@@ -45,37 +64,45 @@ public class DebugPerftTest {
                     throw new IllegalStateException();
                 })
                 .collect(Collectors.toMap(s -> s, s -> 1L, Long::sum));
-        if(!map.containsKey("S")){
-            map.put("S",0L);
+        if (!map.containsKey("S")) {
+            map.put("S", 0L);
         }
-        if(!map.containsKey("E")){
-            map.put("E",0L);
+        if (!map.containsKey("E")) {
+            map.put("E", 0L);
         }
-        if(!map.containsKey("C")){
-            map.put("C",0L);
+        if (!map.containsKey("C")) {
+            map.put("C", 0L);
         }
-        if(!map.containsKey("P")){
-            map.put("P",0L);
+        if (!map.containsKey("P")) {
+            map.put("P", 0L);
         }
         return map;
     }
-    Map<String, Long> add(Map<String, Long> a,Map<String, Long> b){
-        Map<String, Long> result=new HashMap<>();
-        result.put("S",a.get("S")+b.get("S"));
-        result.put("E",a.get("E")+b.get("E"));
-        result.put("C",a.get("C")+b.get("C"));
-        result.put("P",a.get("P")+b.get("P"));
+
+    Map<String, Long> add(Map<String, Long> a, Map<String, Long> b) {
+        Map<String, Long> result = new HashMap<>();
+        result.put("S", a.get("S") + b.get("S"));
+        result.put("E", a.get("E") + b.get("E"));
+        result.put("C", a.get("C") + b.get("C"));
+        result.put("P", a.get("P") + b.get("P"));
         return result;
     }
 
     @Disabled
     @Test
-    void test(){
-        String FEN="r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ";
-        Map<String,Long> result=debugPerft(FENParser.getInstance().parseFEN(FEN),4);
-        System.out.println("S"+result.get("S"));
-        System.out.println("C"+result.get("C"));
-        System.out.println("E"+result.get("E"));
-        System.out.println("P"+result.get("P"));
+    void debugTest() {
+        String FEN = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ";
+        Map<String, Long> result = debugPerft(FENParser.getInstance().parseFEN(FEN), 4);
+        System.out.println("S" + result.get("S"));
+        System.out.println("C" + result.get("C"));
+        System.out.println("E" + result.get("E"));
+        System.out.println("P" + result.get("P"));
+    }
+
+    @Disabled
+    @Test
+    void divideTest() {
+        String FEN = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ";
+        assertEquals(4085603, divide(FENParser.getInstance().parseFEN(FEN), 4, true));
     }
 }
